da949169448e634f5467258e21ea8224b1e3f1a6
Ashish Phal <35269230+ashx56@users.noreply.github.com>
2024-05-03 14:20:57 -0700

    Update FGF_Oligomer_scRNAseq_1.R

--- scRNAseq/FGF_Oligomer_scRNAseq_1.R
+++ scRNAseq/FGF_Oligomer_scRNAseq_1.R
@@ -14,7 +14,7 @@ suppressPackageStartupMessages({
   set.seed(seed = 42)
 })
 
-# Processed data files can be downloaded from Harvard Dataverse DOI: 
+# Processed data files can be downloaded from Harvard Dataverse doi:10.7910/DVN/80RTTV
 # Load raw counts, cell/gene metadata
 counts <- readMM("FGF_Oligomer_scRNAseq_1_filtered_counts.mtx")
 genes <- read.csv("FGF_Oligomer_scRNAseq_1_genes.tsv", header = F)
